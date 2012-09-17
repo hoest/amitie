abb02599930aefc0f59b1cfe819424e0e2a4cc20
Jelle de Jong <jelle@hoest.nl>
2012-09-17 21:34:56 +0200

    Clean up

--- amitie/settings.py
+++ amitie/settings.py
@@ -1,27 +1,20 @@
 # Django settings for amitie project.
-
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
-    )
+  ('Jelle de Jong', 'jelle@hoest.nl'),
+)
 
 MANAGERS = ADMINS
 
 DATABASES = {
   'default': {
-    # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    # Or path to database file if using sqlite3.
     'NAME': 'amitie',
-    # Not used with sqlite3.
     'USER': '',
-    # Not used with sqlite3.
     'PASSWORD': '',
-    # Set to empty string for localhost. Not used with sqlite3.
     'HOST': 'localhost',
-    # Set to empty string for default. Not used with sqlite3.
     'PORT': '',
   }
 }
@@ -62,7 +55,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = 'static/'
+STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -80,7 +73,6 @@ STATICFILES_DIRS = (
 STATICFILES_FINDERS = (
   'django.contrib.staticfiles.finders.FileSystemFinder',
   'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
 # Make this unique, and don't share it with anybody.
@@ -90,7 +82,6 @@ SECRET_KEY = '4zud^m+1ck%g$58s@ol*0h*#o6do*&amp;8q&amp;th)9+k^bsje+!1d9x'
 TEMPLATE_LOADERS = (
   'django.template.loaders.filesystem.Loader',
   'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -99,9 +90,8 @@ MIDDLEWARE_CLASSES = (
   'django.middleware.csrf.CsrfViewMiddleware',
   'django.contrib.auth.middleware.AuthenticationMiddleware',
   'django.contrib.messages.middleware.MessageMiddleware',
-    # Uncomment the next line for simple clickjacking protection:
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    )
+  'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
 
 ROOT_URLCONF = 'amitie.urls'
 
@@ -109,10 +99,10 @@ ROOT_URLCONF = 'amitie.urls'
 WSGI_APPLICATION = 'amitie.wsgi.application'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    )
+  # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+  # Always use forward slashes, even on Windows.
+  # Don't forget to use absolute paths, not relative paths.
+)
 
 INSTALLED_APPS = (
   'django.contrib.auth',
@@ -121,9 +111,7 @@ INSTALLED_APPS = (
   'django.contrib.sites',
   'django.contrib.messages',
   'django.contrib.staticfiles',
-  # Uncomment the next line to enable the admin:
   'django.contrib.admin',
-  # Uncomment the next line to enable admin documentation:
   'django.contrib.admindocs',
   'facebook',
 )
@@ -134,25 +122,25 @@ INSTALLED_APPS = (
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
 LOGGING = {
-'version': 1,
-'disable_existing_loggers': False,
-'filters': {
-'require_debug_false': {
-'()': 'django.utils.log.RequireDebugFalse'
-}
-},
-'handlers': {
-'mail_admins': {
-'level': 'ERROR',
-'filters': ['require_debug_false'],
-'class': 'django.utils.log.AdminEmailHandler'
-}
-},
-'loggers': {
-'django.request': {
-'handlers': ['mail_admins'],
-'level': 'ERROR',
-'propagate': True,
-},
-}
+  'version': 1,
+  'disable_existing_loggers': False,
+  'filters': {
+    'require_debug_false': {
+      '()': 'django.utils.log.RequireDebugFalse'
+    }
+  },
+  'handlers': {
+    'mail_admins': {
+      'level': 'ERROR',
+      'filters': ['require_debug_false'],
+      'class': 'django.utils.log.AdminEmailHandler'
+    }
+  },
+  'loggers': {
+      'django.request': {
+      'handlers': ['mail_admins'],
+      'level': 'ERROR',
+      'propagate': True,
+    },
+  }
 }
